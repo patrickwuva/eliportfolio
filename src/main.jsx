1b5bd559c6d57bc9fc003c0740c455be05980eb7
Patrick Williamson <patrick@truepathvision.com>
2025-04-16 10:55:56 -0400

    updating

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactPlayer from 'react-player'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+<ReactPlayer
+  url="https://drive.google.com/uc?export=download&id=1Zv6hNa-6mBJTW8ZOx4PGdK3mJu15ak30"
+  controls
+  width="100%"
+/>
